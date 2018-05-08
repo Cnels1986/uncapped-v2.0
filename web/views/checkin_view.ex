3e207bc03a4ad3e26b2a53a4739ba653e4e75101
Cnels1986 <CNels1986@gmail.com>
2018-05-07 20:08:57 -0400

    checkin table linked to both the User and the Beer table and displays the correct names, info also displayed for the checkin :show

--- web/views/checkin_view.ex
+++ web/views/checkin_view.ex
@@ -1,3 +1,16 @@
 defmodule Uncapped.CheckinView do
   use Uncapped.Web, :view
+
+  import Ecto.Query
+  alias Uncapped.Repo
+  alias Uncapped.Beer
+  alias Uncapped.User
+
+  def get_beer(id) do
+    Repo.one(from b in Beer, where: b.id == ^id)
+  end
+
+  def get_user(id) do
+    Repo.one(from u in User, where: u.id == ^id)
+  end
 end
